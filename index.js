5fd6a885ed27654ab40d8b00da9eb761221dd16b
Agatha Grig <agathagrigorian@gmail.com>
2018-07-11 20:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var currentUser = "Agatha Grig"
 
 const welcomeMessage = "Welcome to Klossybook, " + (currentUser) + "!"
 
-const excitedWelcomeMessage = "Welcome to Klossybook, " .uppercase
+const excitedWelcomeMessage = "Welcome to Klossybook, " .uppercase;
