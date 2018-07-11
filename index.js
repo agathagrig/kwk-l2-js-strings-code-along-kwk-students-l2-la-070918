a00ec7070e1793dbb30d97f5ff77502f5514fa0b
Agatha Grig <agathagrigorian@gmail.com>
2018-07-11 20:20:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var currentUser = "Agatha Grig"
 
 const welcomeMessage = "Welcome to Klossybook, " + (currentUser) + "!"
 
-const welcomeMessage = "WELCOME TO KLOSSYBOOK, "
+const welcomeMessage = "Welcome to Klossybook, "
